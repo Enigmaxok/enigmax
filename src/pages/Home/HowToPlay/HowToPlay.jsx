c351566e7517ed2ae0fc83b87dcf81c4fc76fcc7
fakumetal <faku.metal@gmail.com>
2024-04-20 11:10:28 -0300

    Mensaje descriptivo del commit

--- src/pages/Home/HowToPlay/HowToPlay.jsx
+++ src/pages/Home/HowToPlay/HowToPlay.jsx
@@ -7,7 +7,7 @@ const HowToPlay = ( ) => {
         <h2><span className="highlightedTitle">¿Cómo</span> se juega?</h2>
         {howToPlayData.map((step, index) => (
   <div  >
-    <div className={index % 2 === 0 ? 'row' : 'row-reverse'} key={index}  >+
+    <div className={index % 2 === 0 ? 'row' : 'row-reverse'} key={index}  >
     <div className={index % 2 === 0 ? 'title-ctn' : 'title-ctn-reverse'}>
       <h3>{step.title}</h3>,
     </div>
