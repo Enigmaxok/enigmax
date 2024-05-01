6c14d5bff2ec7954e366cffa9973c1923e70615c
fakumetal <faku.metal@gmail.com>
2024-05-01 00:34:24 -0300

    Preparar para deploy en GitHub Pages

--- src/Components/Modal/Evento.jsx
+++ src/Components/Modal/Evento.jsx
@@ -38,34 +38,49 @@ const Evento = ({ selectedLocation }) => {
 
   const filteredEvents = selectedLocation ? events.filter(event => event.lugar === selectedLocation) : events;
 
+  const formatDate = (dateString) => {
+    const dateObj = new Date(dateString);
+    const day = dateObj.getDate();
+    const month = dateObj.getMonth() + 1;
+    const formattedDate = `${day}/${month}`;
+    return formattedDate;
+  };
+
+  const formatTime = (timeString) => {
+    const timeObj = new Date(`1970-01-01T${timeString}`);
+    const hours = timeObj.getHours();
+    const minutes = timeObj.getMinutes();
+    const formattedTime = `${hours}:${minutes}`;
+    return formattedTime;
+  };
+
+
   return (
     <div className="modal-body">
       {filteredEvents.map((event, index) => (
-  <div
-  key={index}
-  className={`evento-ctn ${selectedEvent === event ? 'expanded' : ''}`}
-  style={{
-    backgroundColor: event.color,
-    clipPath: index % 2 === 0 ? 'polygon(0% 10%, 100% 0%, 100% 90%, 0% 78%)' : 'polygon(100% 80%, 0% 90%, 0% 0%, 100% 10%)',
-    WebkitClipPath: index % 2 === 0 ? 'polygon(0% 10%, 100% 0%, 100% 90%, 0% 78%)' : 'polygon(100% 80%, 0% 90%, 0% 0%, 100% 10%)',
-    height: selectedEvent === event ? '450px' : '150px', // Altura ajustada según si está seleccionado o no
-  }}
-  onClick={() => handleEventSelect(event)}
->
+      <div
+      key={index}
+      className={`evento-ctn ${selectedEvent === event ? 'expanded' : ''} ${index % 2 === 0 ? 'polygon-1' : 'polygon-2'}`}
+      style={{
+        backgroundColor: event.color,
+        height: selectedEvent === event ? '450px' : '150px', // Altura ajustada según si está seleccionado o no
+      }}
+      onClick={() => handleEventSelect(event)}
+    >
           <div>
             <div className={`evento-content ${selectedEvent === event ? 'expanded' : ''}`}>
               <img className='quien-evento' src={quien} alt="" />
               <img className='detective-evento' src={detective} alt="" />
               <div className='fecha-hora'>
-                <h1>{event.fecha}</h1>
-                <p>{event.hora}</p>
+                <h1>{formatDate(event.fecha)}</h1>
+                <p>{formatTime(event.hora)} hs</p>
               </div>
             </div>
             {selectedEvent === event && (
               <div className="expanded-content">
                 <p><strong>{event.titulo}</strong></p>
-                <p>{event.descripcion}</p>
-                <button  onClick={(e) => handleBuyTicketClick(e)}>  <a href={event.linkCompra} target='_blank' className="comprar-link">Comprar boleto</a></button>
+                <p className="descripcion">{event.descripcion}</p>
+                <button onClick={(e) => handleBuyTicketClick(e)}>  <a href={event.linkCompra} target='_blank' className="comprar-link">Comprar boleto</a></button>
               </div>
             )}
           </div>
